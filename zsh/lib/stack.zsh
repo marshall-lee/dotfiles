d12063520c7f6de36d02800c383ede0e40d4562e
Vladimir Kochnev <hashtable@yandex.ru>
2022-02-10 05:06:58 +0300

    Remove ~/.stack directory if there's no Stack

--- zsh/lib/stack.zsh
+++ zsh/lib/stack.zsh
@@ -1,5 +1,6 @@
 if (( ${+commands[stack]} )) {
   [[ ! -e $ZSH/bash-completions/stack ]] && stack --bash-completion-script stack > $ZSH/bash-completions/stack
-} else {
-  [[ -e $ZSH/bash-completions/stack ]] && rm -f $ZSH/bash-completions/stack
+} elif [[ -e $ZSH/bash-completions/stack ]] {
+  rm -f $ZSH/bash-completions/stack
+  [[ -d $HOME/.stack ]] && rm -rf $HOME/.stack
 }
