aa0ddf4dc0129c2c14f7bf9836ab5d943c10b783
Vladimir Kochnev <hashtable@yandex.ru>
2022-08-18 02:09:46 +0300

    Re-hash rbenv in background

--- zsh/init/rbenv.zsh
+++ zsh/init/rbenv.zsh
@@ -4,10 +4,11 @@ function my_rbenv_init() {
   local rbenv_cache="$ZSH/cache/rbenv-init"
   if [[ "$commands[rbenv]" -nt "$rbenv_cache" || ! -s $rbenv_cache ]] {
     echo 'Generating rbenv initialization script...'
-    rbenv init - zsh >| $rbenv_cache
+    rbenv init - --no-rehash zsh >| $rbenv_cache
     zcompile $rbenv_cache
   }
   source $rbenv_cache
+  rbenv rehash 2>/dev/null &|
 }
 
 my_rbenv_init
