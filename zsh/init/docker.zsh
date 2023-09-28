5879760f387b55513e31c20dd3af4efd50c70e8e
Vladimir Kochnev <hashtable@yandex.ru>
2023-09-28 23:10:58 +0300

    Fix docker completions symlinking
    Docker Desktop regression: https://github.com/docker/for-mac/issues/6992

--- zsh/init/docker.zsh
+++ zsh/init/docker.zsh
@@ -1,6 +1,11 @@
-# Symlink Docker Desktop completion files.
-# https://docs.docker.com/desktop/mac/#install-shell-completion
-if [[ "$OSTYPE" == darwin* ]] && [[ -e /Applications/Docker.app ]] {
-  [[ ! -e $ZSH/completions/_docker ]] && ln -s /Applications/Docker.app/Contents/Resources/etc/docker.zsh-completion $ZSH/completions/_docker
-  [[ ! -e $ZSH/completions/_docker-compose ]] && ln -s /Applications/Docker.app/Contents/Resources/etc/docker-compose.zsh-completion $ZSH/completions/_docker-compose
+function my_docker_init() {
+  # Symlink Docker Desktop completion files.
+  # https://docs.docker.com/desktop/faqs/macfaqs/#zsh
+  if [[ "$OSTYPE" == darwin* ]] && [[ -e /Applications/Docker.app ]] {
+    local docker_app_etc=/Applications/Docker.app/Contents/Resources/etc
+    [[ ! -e $ZSH/completions/_docker && -e $docker_app_etc/docker.zsh-completion ]] && ln -s $docker_app_etc/docker.zsh-completion $ZSH/completions/_docker
+    [[ ! -e $ZSH/completions/_docker-compose && -e $docker_app_etc/docker-compose.zsh-completion ]] && ln -s $docker_app_etc/docker-compose.zsh-completion $ZSH/completions/_docker-compose
+  }
 }
+
+my_docker_init
