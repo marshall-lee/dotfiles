19d77b4a5a2fa3086640ebb0676b061f69be6fd3
Vladimir Kochnev <hashtable@yandex.ru>
2025-03-10 04:40:12 +0300

    Add rsync-cp

--- zsh/init/rsync.zsh
+++ zsh/init/rsync.zsh
@@ -1,8 +1,8 @@
-function fswatch-rsync() {
+function rsync-fswatch() {
   local local_path=$1
   local remote_path=$2
   if [[ -z $local_path || -z $remote_path ]] {
-    echo "Usage: fswatch-rsync <local-path> <remote-path>" >&2
+    echo "Usage: rsync-fswatch <local-path> <remote-path>" >&2
     return -1
   }
   # Sync all.
@@ -10,4 +10,15 @@ function fswatch-rsync() {
 
   # Watch for changes & sync.
   fswatch -o ${local_path} | xargs -I{} rsync --delete -vrlpt ${local_path} ${remote_path}
+}
+
+function rsync-cp() {
+  local local_path=$1
+  local remote_path=$2
+  if [[ -z $local_path || -z $remote_path ]] {
+    echo "Usage: rsync-cp <local-path> <remote-path>" >&2
+    return -1
+  }
+
+  rsync -vrlp ${local_path} ${remote_path}
 }
