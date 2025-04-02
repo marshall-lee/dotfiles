67d2dc1584b72d409672b50c73216fb89c25c977
Vladimir Kochnev <hashtable@yandex.ru>
2025-04-02 21:11:09 +0300

    Rename local/remote to src/dst

--- zsh/init/rsync.zsh
+++ zsh/init/rsync.zsh
@@ -1,24 +1,24 @@
 function rsync-fswatch() {
-  local local_path=$1
-  local remote_path=$2
-  if [[ -z $local_path || -z $remote_path ]] {
-    echo "Usage: rsync-fswatch <local-path> <remote-path>" >&2
+  local src_path=$1
+  local dst_path=$2
+  if [[ -z $src_path || -z $dst_path ]] {
+    echo "Usage: rsync-fswatch <src-path> <dst-path>" >&2
     return -1
   }
   # Sync all.
-  rsync --delete -vrlpt ${local_path} ${remote_path}
+  rsync --delete -vrlpt ${src_path} ${dst_path}
 
   # Watch for changes & sync.
-  fswatch -o ${local_path} | xargs -I{} rsync --delete -vrlpt ${local_path} ${remote_path}
+  fswatch -o ${src_path} | xargs -I{} rsync --delete -vrlpt ${src_path} ${dst_path}
 }
 
 function rsync-cp() {
-  local local_path=$1
-  local remote_path=$2
-  if [[ -z $local_path || -z $remote_path ]] {
-    echo "Usage: rsync-cp <local-path> <remote-path>" >&2
+  local src_path=$1
+  local dst_path=$2
+  if [[ -z $src_path || -z $dst_path ]] {
+    echo "Usage: rsync-cp <src-path> <dst-path>" >&2
     return -1
   }
 
-  rsync -vrlp ${local_path} ${remote_path}
+  rsync -vrlp ${src_path} ${dst_path}
 }
