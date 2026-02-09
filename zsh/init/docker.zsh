4e4fc150c1e31a4ef766683510519588788e3b14
Vladimir Kochnev <hashtable@yandex.ru>
2026-02-09 17:28:25 +0300

    Fix _docker and _docker-compose check once again

--- zsh/init/docker.zsh
+++ zsh/init/docker.zsh
@@ -4,11 +4,11 @@ function my_docker_init() {
   if [[ "$OSTYPE" == darwin* ]] &&  {
     local docker_app_etc=/Applications/Docker.app/Contents/Resources/etc
     if [[ -e /Applications/Docker.app ]] {
-      [[ ! -e $ZSH/completions/_docker && -e $docker_app_etc/docker.zsh-completion ]] && ln -s $docker_app_etc/docker.zsh-completion $ZSH/completions/_docker
-      [[ ! -e $ZSH/completions/_docker-compose && -e $docker_app_etc/docker-compose.zsh-completion ]] && ln -s $docker_app_etc/docker-compose.zsh-completion $ZSH/completions/_docker-compose
+      [[ (! -L $ZSH/completions/_docker) && (! -e $ZSH/completions/_docker) && -f $docker_app_etc/docker.zsh-completion ]] && ln -s $docker_app_etc/docker.zsh-completion $ZSH/completions/_docker
+      [[ (! -L $ZSH/completions/_docker-compose) && (! -e $ZSH/completions/_docker) && -f $docker_app_etc/docker-compose.zsh-completion ]] && ln -s $docker_app_etc/docker-compose.zsh-completion $ZSH/completions/_docker-compose
     } else {
-      [[ -e $ZSH/completions/_docker ]] && [[ $(readlink $ZSH/completions/_docker) == $docker_app_etc/docker.zsh-completion ]] && rm -f $ZSH/completions/_docker
-      [[ -e $ZSH/completions/_docker-compose ]] && [[ $(readlink $ZSH/completions/_docker-compose) == $docker_app_etc/docker-compose.zsh-completion ]] && rm -f $ZSH/completions/_docker-compose
+      [[ -L $ZSH/completions/_docker ]] && [[ $(readlink $ZSH/completions/_docker) == $docker_app_etc/docker.zsh-completion ]] && rm -f $ZSH/completions/_docker
+      [[ -L $ZSH/completions/_docker-compose ]] && [[ $(readlink $ZSH/completions/_docker-compose) == $docker_app_etc/docker-compose.zsh-completion ]] && rm -f $ZSH/completions/_docker-compose
     }
   }
 }
