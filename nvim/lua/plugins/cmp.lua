b76a0e7b261103f42796b16ed4913158390d167b
Vladimir Kochnev <hashtable@yandex.ru>
2026-04-13 12:37:50 +0300

    nvim: set up completion key mappings

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -8,7 +8,15 @@ return {
       "hrsh7th/cmp-path"
     },
     config = function()
-      require("cmp").setup({
+      local cmp = require("cmp")
+      cmp.setup({
+        mapping = cmp.mapping.preset.insert({
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        }),
         sources = {
           { name = "nvim_lsp" },
           { name = "buffer" },
