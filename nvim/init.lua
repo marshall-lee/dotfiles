06aaf563e1f9e4b8b11873971303674c0dcffb15
Vladimir Kochnev <hashtable@yandex.ru>
2026-04-18 02:03:02 +0300

    nvim: tune telescope bindings

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,6 @@
 require("config.lazy")
 require("config.indent")
 require("config.langmap")
-require("config.telescope")
 require("config.python3")
 
 vim.cmd[[colorscheme solarized-osaka]]
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -18,5 +18,14 @@ return {
   config = function()
     local telescope = require("telescope")
     telescope.load_extension('fzf')
+
+    local builtin = require("telescope.builtin")
+    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+    vim.keymap.set("n", "<leader>fr", function() builtin.oldfiles({ initial_mode = "normal" }) end, { desc = 'Telescope recent files' })
+    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+    vim.keymap.set('n', '<leader>fw', builtin.grep_string, { desc = 'Telescope search word under cursor' })
+    vim.keymap.set('n', '<leader>fb', function() builtin.buffers({ initial_mode = "normal" }) end, { desc = 'Telescope buffers' })
+    vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+    vim.keymap.set("n", "<leader>tr", function() builtin.resume({ initial_mode = "normal" }) end, { desc = 'Telescope resume' })
   end,
 }
