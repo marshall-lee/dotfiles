7cccb0f6159c6a1b673ad49ddf0655a5341c37d3
Vladimir Kochnev <hashtable@yandex.ru>
2026-05-11 20:11:10 +0300

    nvim: add fG key binding to live grep in a git repo

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -24,6 +24,19 @@ return {
     vim.keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = 'Telescope recent files' })
     vim.keymap.set("n", "<leader>fR", function() builtin.oldfiles({ initial_mode = "normal" }) end, { desc = 'Telescope recent files (normal mode)' })
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+    vim.keymap.set('n', '<leader>fG', function()
+      local Path = require("plenary.path")
+      local path = Path:new(vim.fn.expand('%:p'))
+      if not path:exists() then path = Path:new(vim.loop.cwd()) end
+      local gitroot = vim.tbl_filter(function(parent)
+        return Path:new(parent .. '/.git'):is_dir()
+      end, path:parents())[1]
+      if gitroot then
+        builtin.live_grep({ cwd = gitroot })
+      else
+        vim.notify("git repo is not found", vim.log.levels.ERROR)
+      end
+    end, { desc = 'Telescope live grep (git repo)' })
     vim.keymap.set('n', '<leader>fw', builtin.grep_string, { desc = 'Telescope search word under cursor' })
     vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
     vim.keymap.set('n', '<leader>fB', function() builtin.buffers({ initial_mode = "normal" }) end, { desc = 'Telescope buffers (normal mode)' })
