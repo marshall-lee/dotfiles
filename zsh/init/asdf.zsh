3c7d29afff7f4cbb34ebfcf529296773066744cd
Vladimir Kochnev <hashtable@yandex.ru>
2026-08-13 21:09:16 +0300

    zsh: fix asdf integration

--- zsh/init/asdf.zsh
+++ zsh/init/asdf.zsh
@@ -1,11 +1,7 @@
 function my_asdf_init() {
-  local asdf
-
-  if [[ -n $HOMEBREW_PREFIX ]] && [[ -e $HOMEBREW_PREFIX/opt/asdf/libexec/asdf.sh ]] {
-    asdf=$HOMEBREW_PREFIX/opt/asdf/libexec/asdf.sh
+  if (( ${+commands[asdf]} )) {
+    path=("${HOME}/.asdf/shims" $path)
   }
-
-  if [[ -n $asdf ]] && source $asdf
 }
 
 my_asdf_init
